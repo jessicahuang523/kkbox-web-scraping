d4388f43a7a2b2ce4ba0990cd66ff27acf83c0da
yungchieh <yungchieh@jessicade-MacBook-Pro.local>
2019-08-04 21:57:26 +0800

    final

--- kkboxscraping.py
+++ kkboxscraping.py
@@ -1,10 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import json
 
 def getLink(url):
     html = urlopen(url)
     bs = BeautifulSoup(html)
     return bs
+
+def writeJson(data):
+    with open("output.json", "w") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+        file.close
 
 def scrapePage(link):
     page = getLink(link)
@@ -12,19 +19,34 @@ def scrapePage(link):
     for article in articles:
         news = getLink("http://www.kkbox.com"+article.attrs['href'])
         title = news.find("h1")
+        author = news.find("div", {"class": "media-heading"}).find("span")
         contents = news.find("div", {"class": "column-article"}).findAll("p")
         keywords = news.findAll("li", {"class": "keyword"})
-        print(">>>title<<<")
-        print(title.get_text())
-        print(">>>keyword<<<")
-        for keyword in keywords:
-            print(keyword.get_text())
-        print(">>>content<<<")
+        newData = {}
+        newData['標題'] = title.get_text()
+        newData['作者'] = author.get_text()
+        contentArray = []
         for content in contents:
-            print(content.get_text())
-        print("------------------------")
+            if content.find("img") or content.find("iframe") or content.find("a"):
+                continue
+            contentArray.append(content.get_text())
+        newContent = ''.join(contentArray)
+        newData['內容'] = newContent
+        newData['關鍵字'] = []
+        for keyword in keywords:
+            if keyword.find("a"):
+                continue
+            newData['關鍵字'].append(keyword.get_text())
+        newData['meta'] = {}
+        metaTags = news.findAll("meta", {"property": re.compile("^(og:)")})
+        for metaTag in metaTags:
+            newData['meta'][metaTag.attrs['property']] = metaTag.attrs['content']
+        data['音樂頭條'].append(newData)
+data = {}
+data['音樂頭條'] = []
 
 home = "https://www.kkbox.com/hk/tc/column/index.html"
-scrapePage(home)
-for noPage in range(2, 4):
+for noPage in range(1, 4):
     scrapePage(home + "?p=" + str(noPage))
+
+writeJson(data)
